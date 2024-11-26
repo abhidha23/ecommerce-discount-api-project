61b2eec97e0a2eff5d2ae135a7e526e8433c90a0
Abhidha Sharma <123157831+abhidha23@users.noreply.github.com>
2024-11-26 08:47:57 +0530

    Create Product model for product table (#2)
    Added SQLAlchemy model for  table with columns , , and .
    -  is a UUID primary key.
    
     is a UUID primary key.
    
     and  are required fields for each product.

--- app/models/products.py
+++ app/models/products.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime
 from sqlalchemy import JSON
 from datetime import datetime
-from database import Base
+from app.database import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
-class Product(Base):
+class Products(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    price = Column(Float, nullable=False)
+    product_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    product_name = Column(String(100), nullable=False)
+    product_price = Column(Float, nullable=False)
+    created_at = Column(DateTime, nullable=False)
