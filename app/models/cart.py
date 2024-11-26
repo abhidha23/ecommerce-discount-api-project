b177f29c51e4a48a4bd8e4a74f4dd9c3ada98c1b
Abhidha Sharma <fwl7032@gmail.com>
2024-11-26 08:27:37 +0530

    Added Cart Table Model with Basic Fields for Cart Management

--- app/models/cart.py
+++ app/models/cart.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime
 from sqlalchemy import JSON
 from datetime import datetime
-from database import Base  
+from app.database import Base  
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 class Cart(Base):
-    __tablename__ = 'carts'
+    __tablename__ = 'cart'
 
-    id = Column(Integer, primary_key=True, index=True)
-    total_amount = Column(Float, nullable=False)
-    products = Column(JSON, nullable=False) 
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    cart_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    cart_quantity = Column(Integer, default=0) 
+    total_amount = Column(Float, default=0.0) 
